883381195515ce7da0ad7efa593d7128abc0fb74
Pranav Takrani <pranav.takrani@gmail.com>
2025-07-16 04:45:48 +0530

    fuck this shit provider doesnt work

--- cal_track_app/lib/main.dart
+++ cal_track_app/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'auth_page.dart';
 
-void main() {
-  runApp(const CalorieTrackApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://gktjlubvxzcbceqmfvkj.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdGpsdWJ2eHpjYmNlcW1mdmtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2MTg2ODksImV4cCI6MjA2ODE5NDY4OX0.VYhgnj-tTaQ9zbNwVaJ3hB-xe4_QSLCnqqADYjSeA9I',
+  );
+  runApp(const ProviderScope(child: CalorieTrackApp()));
 }
 
 class CalorieTrackApp extends StatelessWidget {
@@ -12,11 +20,7 @@ class CalorieTrackApp extends StatelessWidget {
     return MaterialApp(
       title: 'Calorie Track App',
       theme: ThemeData(primarySwatch: Colors.green),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello Calorie Track App!'),
-        ),
-      ),
+      home: const AuthPage(),
     );
   }
 }
