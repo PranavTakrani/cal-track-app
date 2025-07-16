e6f01eb196690cc01f1145c26eba34a215b2b9a1
Pranav Takrani <pranav.takrani@gmail.com>
2025-07-16 14:13:31 +0530

    prefix error for supa vs flltr fixed

--- cal_track_app/lib/auth_provider.dart
+++ cal_track_app/lib/auth_provider.dart
@@ -1,11 +1,11 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' as riverpod;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-final supabaseProvider = Provider<SupabaseClient>((ref) {
+final supabaseProvider = riverpod.Provider<SupabaseClient>((ref) {
   return Supabase.instance.client;
 });
 
-final authStateProvider = StreamProvider<AuthState>((ref) {
+final authStateProvider = riverpod.StreamProvider<Session?>((ref) {
   final supabase = ref.watch(supabaseProvider);
   return supabase.auth.onAuthStateChange.map((event) => event.session);
 });
